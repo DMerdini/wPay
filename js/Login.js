46ca15bb9b7558c1a7115612610aff108601d253
Devis Merdini <devismerdini@hotmail.com>
2025-02-26 12:33:27 +0100

    modified:   css/login.css 	modified:   html/login.html 	modified:   js/Login.js

--- js/Login.js
+++ js/Login.js
@@ -1,1 +1,25 @@
 //
+
+let sysusers = [
+  {
+    // later change username with email
+    sysusername: "pablo",
+    sysuserpass: "123",
+    accdetails: {
+      accusename: "Pablo",
+      accuserlastname: "Picasso",
+      accuserpic: "",
+    },
+    acccards: [{ acccardname: "", acccardiban: "", acccardstatus: "" }],
+    accbills: [{ accbillname: "", accbillamount: "", accbillstatus: "" }],
+  },
+];
+
+function checkauth() {
+  let enteredusername = document.getElementById("enteredemail").value.trim();
+  let enteredpass = document.getElementById("enteredpass").value.trim();
+  let founduser = sysusers.find((user) => {
+    return user.sysusername.toLowerCase() === enteredusername.toLowerCase();
+  });
+  localStorage.getItem("userfound", JSON.stringify(founduser));
+}
